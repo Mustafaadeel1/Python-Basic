d232909a160815bfddd3b0d1d84eb192dcba5825
Mustafaadeel1 <mustafaadeel989@gmail.com>
2024-09-02 11:37:25 -0700

    add repo

---
+++ Basic-assi-no-3.py
@@ -0,0 +1,36 @@
+#Question no 1 = index[] :Forward
+name = "Mustafa"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+
+#Question no 2 = slicing[start:end]
+sentense  = "My name is Mustafa , I read in 10th class , My city name is Sialkot "
+print(sentense[0:18])
+print(sentense[21:41])
+print(sentense[44:67])
+
+#Question no 3 = step [start : end : step]
+jump_value = "My name is Mustafa , I read in 10th class , My city name is Sialkot , I love my countary "
+print(jump_value[0:18:2])
+print(jump_value[0:34:3])
+print(jump_value[0:55:4])
+print(jump_value[0:70:5])
+print(jump_value[0:70:6])
+
+#Qustion no 4 = Reverse method 
+back = "My name is Mustafa , I read in 10th class , My city name is Sialkot , I love my countary "
+print(back[::-1])
+print(back[::-2])
+print(back[::-3])
+print(back[::-4])
+print(back[::-5])
+print(back[::-6])
+print(back[::-7])
+print(back[::-8])
+print(back[::-9])
+print(back[::-10])
