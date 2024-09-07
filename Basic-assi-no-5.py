89c4af49a801354c948dad0c8faa1e988a4f0bfd
Mustafaadeel1 <mustafaadeel989@gmail.com>
2024-09-07 09:07:24 -0700

    add file

---
+++ Basic-assi-no-5.py
@@ -0,0 +1,134 @@
+#String types:
+# String convert into Lowercase():
+name = "Hello! MUSTAFA"
+print(name.lower())
+say = "WHAT IS YOUR NAME?"
+print(say.lower())
+awn = "MY NAME IS MUSTAFA"
+print(awn.lower())
+name = "Hello! MUSTAFA"
+print(name.lower())
+say = "WHAT IS YOUR NAME?"
+print(say.lower())
+awn = "MY NAME IS MUSTAFA"
+print(awn.lower())
+
+#String convert into uppercase():
+
+name = "Hello! mustafa"
+print(name.upper())
+say = "what is your name"
+print(say.upper())
+awn = "my name is mustafa"
+print(awn.upper())
+name = "hello! mustafa"
+print(name.upper())
+say = "what is your name?"
+print(say.upper())
+awn = "my name is mustafa"
+print(awn.upper())
+
+#String convert into title():
+
+name = "Hello! mustafa"
+print(name.title())
+say = "what is your name"
+print(say.title())
+awn = "my name is mustafa"
+print(awn.title())
+name = "hello! mustafa"
+print(name.title())
+say = "what is your name?"
+print(say.title())
+awn = "my name is mustafa"
+print(awn.title())
+
+#String convert into capitalize:
+
+name = "Hello! mustafa"
+print(name.capitalize())
+say = "what is your name"
+print(say.capitalize())
+awn = "my name is mustafa"
+print(awn.capitalize())
+name = "hello! mustafa"
+print(name.capitalize())
+say = "what is your name?"
+print(say.capitalize())
+awn = "my name is mustafa"
+print(awn.capitalize())
+
+#String change into casefold:
+
+name = "Hello! mustafa"
+print(name.casefold())
+say = "what is your name"
+print(say.casefold())
+awn = "my name is mustafa"
+print(awn.casefold())
+name = "hello! mustafa"
+print(name.casefold())
+say = "what is your name?"
+print(say.casefold())
+awn = "my name is mustafa"
+print(awn.casefold())
+
+#String change into center:
+
+name = "Hello! mustafa"
+print(name.center(21,"*"))
+say = "what is your name"
+print(say.center(21,"("))
+awn = "my name is mustafa"
+print(awn.center(21,"!"))
+name = "hello! mustafa"
+print(name.center(21,"/"))
+say = "what is your name?"
+print(say.center(21,","))
+awn = "my name is mustafa"
+print(awn.center(21,"."))
+
+#String change into endswith:
+
+name = "Hello! mustafa"
+print(name.endswith("a"))
+say = "what is your name"
+print(say.endswith('e'))
+awn = "my name is mustafa"
+print(awn.endswith("a"))
+name = "hello! mustafa"
+print(name.endswith("h"))
+say = "what is your name?"
+print(say.endswith("e?"))
+awn = "my name is mustafa"
+print(awn.endswith("a"))
+
+#String change into find:
+
+name = "Hello! mustafa"
+print(name.find("fa"))
+say = "what is your name"
+print(say.find("na"))
+awn = "my name is mustafa"
+print(awn.find("is"))
+name = "hello! mustafa"
+print(name.find("he"))
+say = "what is your name?"
+print(say.find("your"))
+awn = "my name is mustafa"
+print(awn.find("st"))
+
+#String change into expandtabs:
+
+name = "Hello! \tmustafa"
+print(name.expandtabs(10))
+say = "what is \tyour name"
+print(say.expandtabs(11))
+awn = "my name \tis mustafa"
+print(awn.expandtabs(12))
+name = "hello! \tmustafa"
+print(name.expandtabs(13))
+say = "what is \tyour name?"
+print(say.expandtabs(14))
+awn = "my name \tis mustafa"
+print(awn.expandtabs(15))
