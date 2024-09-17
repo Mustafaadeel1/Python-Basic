67025febdefb15be2c302e771b54718627293251
Mustafaadeel1 <mustafaadeel989@gmail.com>
2024-09-17 01:45:11 -0700

    add if

---
+++ Basic-assi-no-6.py
@@ -0,0 +1,21 @@
+i= int (input("Enter your marks:"))
+if (i > 90):
+    print("A+")
+elif (i > 80):
+    print("A")
+elif (i > 70):
+    print("B+")
+elif(i > 60):
+    print("B")
+elif (i > 50):
+    print("C+")
+elif (i > 40):
+    print("C")
+elif (i > 30):
+    print("D+")
+elif (i > 20):
+    print("D")
+elif (i > 10):
+    print("E")
+else:
+    print("Say you are Fail, Better Luck next time.")                                
