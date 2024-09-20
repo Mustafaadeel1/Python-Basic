3741bb540975a33c211628fac5cb99a1ca0a43c9
Mustafaadeel1 <mustafaadeel989@gmail.com>
2024-09-20 09:25:28 -0700

    ADD LINE

--- Basic-assi-no-7.py
+++ Basic-assi-no-7.py
@@ -1,6 +1,6 @@
 # Army test minimum requirement: 
 mini_age: int = 18
-mini_Qualification: str ="2nd year"
+mini_Qualification: str = ("2nd year")
 mini_hight : float = 5.10
 age = int(input("Enter your age: "))
 qualification = input("Enter your Qualification (e.g., '2nd year'): ")
@@ -8,9 +8,9 @@ height = float(input("Enter your height in feet: "))
 
 if age >= mini_age:
     print("Your age is 18+ you are Pass.") 
-    if qualification  >= mini_Qualification:
+    if qualification== mini_Qualification:
         print("Your Qualification 2nd above your are Pass.")
-        if height >=mini_hight:
+        if height >=mini_hight:     
             print("Your hight is above 5.10 you are Pass.")
         else:
             print("Your hight is not above 5.10 you are Fail.")    
