537a8ffb33541a8560db3c726c72c4c3a0250dea
Mustafaadeel1 <mustafaadeel989@gmail.com>
2024-10-08 06:13:12 -0700

    add logic

--- Basic-assi-no-12.py
+++ Basic-assi-no-12.py
@@ -1,28 +1,4 @@
-# user_start = input("'Assalamu Alaikum' You can start new Journey after your Death. To Run Journey, say 'start': ")  
-
-# if user_start == "start":
-#     print("Feel you are in Grave...")
-#     print("Munker and Nakir have come to question you...")
-    
-#     question = input("Who is Your God? ").lower().strip()#Remove any spaces
-    
-#     if question == "Allah":
-#         print("Well done 'Mashallah'")
-#         question2 : str = input("Who is your Prophet ?").lower().strip()
-#         question2 =="Muhmmad"
-#         print("Mashallah Well done..")
-#         question3 : str = input("What is your Religion ?")
-#         question3 == "Islam"
-#         print("Mashallah 'Welcome to 'Jannah'")
-#         print("'Assalamu Alaikum'")
-#         question3 == "end"
-#         break
-#     else:   
-#         print("You are Fail. Now you are going to hell.")
-# else:
-#     print("Its your choice---If you start Its your own choice you say 'No' Its you choice")
-
-        
+# This is my Practices To develope my Logic:
 def start_journey():
     user_start = input("'Assalamu Alaikum' You can start a new Journey after your Death. To Run Journey, say 'start': ").lower().strip()
 
@@ -47,7 +23,7 @@ def start_journey():
                     if end_journey == "end":
                         print("Journey ended.")
                     else:
-                        print("Continuing journey...")  # You can handle other cases if needed
+                        print("Continuing journey...") 
                 else:
                     print("Incorrect answer for Religion. You have failed.")
             else:
@@ -57,6 +33,4 @@ def start_journey():
     else:
         print("It's your choice. If you start, it's your own decision. If you say 'no', that's your choice.")
 
-
-# Call the function to start the journey
 start_journey()
